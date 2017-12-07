bd8a5ef472a6d17d998099de5db3631b44a2f04a
Kirat1 <kirat2611@gmail.com>
2017-12-07 15:15:09 -0500

    Add files via upload

---
+++ Day3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Day3
+    {
+        //Part1
+        public static void Main(string[] args)
+        {
+            var x = 277678;
+            var y = 1;
+            var count = 0;
+            while(y<x)
+            {
+                y = y + 8 * count;
+                count++;
+            }
+            //because we start from 0,0
+            var x1 = count-1;
+            Console.WriteLine("Val y {0}", y);
+            Console.WriteLine("Val count {0}", count);
+            var diff = y - x;
+            Console.WriteLine("Diff is {0}", diff);
+            
+            var x2 = x1 - diff;
+            var distance = x1 + x2;
+            Console.WriteLine("Distance is {0}", distance);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day3Part2.js
@@ -0,0 +1,32 @@
+﻿
+    var data = 277678;
+    var x, y, matrix;
+    x = y = 0;
+    matrix = {};
+    matrix[x + ',' + y] = 1;
+    while(true) {
+        var val = getValue(matrix, x, y);
+        if (val >= data) {
+            console.log(val);
+            break;
+        }
+        matrix[x + ',' + y] = val;
+
+        if ((x !== y || x >= 0) && Math.abs(x) <= Math.abs(y)) {
+            x += y >= 0 ? 1 : -1;
+        } else {
+            y += x >= 0 ? -1 : 1;
+        }
+    }
+
+function getValue(matrix, posX, posY) {
+    var sum = 0;
+    for (var x = posX - 1; x <= posX + 1; x++) {
+        for(var y = posY - 1; y <= posY + 1; y++) {
+            if (matrix[x + ',' + y]) {
+                sum += matrix[x + ',' + y];
+            }
+        }
+    }
+    return sum;
+}
