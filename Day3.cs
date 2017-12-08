30f59473b529a75953c9f4a077b34d8740566ffc
Kirat1 <kirat2611@gmail.com>
2017-12-08 10:42:28 -0500

    Update Day3.cs

--- Day3.cs
+++ Day3.cs
@@ -11,25 +11,29 @@ namespace ConsoleApp1
         //Part1
         public static void Main(string[] args)
         {
-            var x = 277678;
+            var x = 1024;
             var y = 1;
             var count = 0;
-            while(y<x)
+            while (y < x)
             {
                 y = y + 8 * count;
-                count++;
+                if(y<x)
+                    count++;
             }
             //because we start from 0,0
-            var x1 = count-1;
-            Console.WriteLine("Val y {0}", y);
-            Console.WriteLine("Val count {0}", count);
+            var x1 = count;
             var diff = y - x;
-            Console.WriteLine("Diff is {0}", diff);
-            
+            if (diff > count)
+            {
+                diff = diff/2 -1;
+            }
             var x2 = x1 - diff;
+            if (x2 < 0)
+                x2 *= -1;
             var distance = x1 + x2;
             Console.WriteLine("Distance is {0}", distance);
             Console.ReadKey();
+
         }
     }
 }
